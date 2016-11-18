46295754f9b44d62e7265fe86eb4f852a0ee6918
midhun4munna <midhun4munna@gmail.com>
2016-11-18 18:10:58 +0530

    v2

--- angular-week2-sample/app/app.module.ts
+++ angular-week2-sample/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { AppRoutingModule,routingComponents }   from './app.routing';
 import { SearchBox }   from './searchbox.component';
+import { HeroService } from './hero.service';
+import { HttpModule }    from '@angular/http';
 
 @NgModule({
-  imports:      [ BrowserModule,AppRoutingModule ],
+  imports:      [ BrowserModule,AppRoutingModule,HttpModule  ],
   declarations: [ AppComponent,routingComponents,SearchBox ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [HeroService]
 })
 
 export class AppModule { }
