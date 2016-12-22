c4cb90a0279f7f37915db206c0e81094aaa90062
midhun4munna <midhun4munna@gmail.com>
2016-12-22 14:32:38 +0530

    Update ClaimDAO.java

--- AlexaClaimInfoApp/src/main/java/dao/ClaimDAO.java
+++ AlexaClaimInfoApp/src/main/java/dao/ClaimDAO.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 public class ClaimDAO {
 	private static final Logger log = LoggerFactory.getLogger(ClaimDAO.class);
 
@@ -18,18 +16,21 @@ public class ClaimDAO {
 	private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/claims";
 	private static final String DB_USER = "root";
 	private static final String DB_PASSWORD = "root";
+	private static Connection con = null;
 
 	public HashMap<Integer, String> fetchClaims() {
 		System.out.println("Starting fetchClaims");
 		HashMap<Integer, String> userClaimMap = new HashMap<Integer, String>();
-
+		ResultSet rs = null;
+		Statement statement = null;
+		Connection connection = null;
 		try {
-			Connection connection = getDBConnection();
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery("select * from user_claims where usercode='Midhun'");
+			connection = getDBConnection();
+			statement = connection.createStatement();
+			rs = statement.executeQuery("select * from user_claims where usercode='Midhun'");
 			int index = 0;
 			while (rs.next()) {
-				System.out.println("user_claim  claimnumber : "+rs.getString("claimId"));
+				System.out.println("user_claim  claimnumber : " + rs.getString("claimId"));
 				userClaimMap.put(++index, rs.getString("claimId"));
 			}
 			System.out.println("Completed fetchClaims");
@@ -37,6 +38,17 @@ public class ClaimDAO {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (SQLException se) {
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (SQLException se) {
+			}
 		}
 		return userClaimMap;
 	}
@@ -44,14 +56,16 @@ public class ClaimDAO {
 	public String fetchClaimDetails(String claimNumber) {
 		System.out.println("Starting fetchClaimDetails for ClaimNumber " + claimNumber);
 		HashMap<Integer, String> userClaimMap = new HashMap<Integer, String>();
+		ResultSet rs = null;
+		Statement statement = null;
+		Connection connection = null;
 		String claimDetails = null;
 		String claimStatus = null;
 		String claimDiscription = null;
 		try {
-			Connection connection = getDBConnection();
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement
-					.executeQuery("select * from claim_details where claimId  = '" + claimNumber + "'");
+			connection = getDBConnection();
+			statement = connection.createStatement();
+			rs = statement.executeQuery("select * from claim_details where claimId  = '" + claimNumber + "'");
 			while (rs.next()) {
 				claimStatus = rs.getString("claimStatus");
 				claimDiscription = rs.getString("description");
@@ -61,14 +75,27 @@ public class ClaimDAO {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (SQLException se) {
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (SQLException se) {
+			}
 		}
 		System.out.println("Completed fetchClaimDetails : " + claimDetails);
 		return claimDetails;
 	}
 
 	private Connection getDBConnection() throws ClassNotFoundException, SQLException {
-		Class.forName(DB_DRIVER);
-		Connection con = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+		if (con == null) {
+			Class.forName(DB_DRIVER);
+			con = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+		}
 		return con;
 	}
 
