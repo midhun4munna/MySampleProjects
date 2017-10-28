bfa7d6cb58acfad3aeb35466d406e963c62cbc24
midhun4munna <midhun4munna@gmail.com>
2017-10-28 09:45:45 +0000

    Todo app

---
+++ todo-Springboot/src/main/java/com/example/demo/TodoDAO.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TodoDAO {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void create(ToDoModel apiRequest) {
+		entityManager.persist(apiRequest);
+	}
+
+	public void update(ToDoModel apiRequest) {
+		entityManager.merge(apiRequest);
+	}
+
+	public ToDoModel getToDoModelById(int id) {
+		return entityManager.find(ToDoModel.class, id);
+	}
+
+	public void delete(int id) {
+		ToDoModel todomodel = getToDoModelById(id);
+		if (todomodel != null) {
+			entityManager.remove(todomodel);
+		}
+	}
+	
+	public List<ToDoModel> getAll() {
+		 List<ToDoModel> toDoList = (List<ToDoModel>)entityManager.createQuery( "SELECT t FROM ToDoModel t").getResultList();
+		 return toDoList;
+	}
+
+}
